f26f073cc7c863fe9c7d1e0e4dd8bad34c8c00a6
EroAuditore <alfa.fenrir@gmail.com>
2021-03-23 16:24:11 -0600

    (Fix) user controller user_params permit

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,6 +12,6 @@ class UsersController < ApplicationController
     end
 
     def user_params
-        params.require(:user).permit(:username,:email,:password)
+        params.permit(:username,:email,:password)
     end
 end
