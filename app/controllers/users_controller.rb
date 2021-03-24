f31112bfaf98cfd8113776ec7272c20513b3702e
EroAuditore <alfa.fenrir@gmail.com>
2021-03-24 10:42:45 -0600

    Added edit form user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,6 +12,17 @@ class UsersController < ApplicationController
         end   
     end
 
+    def edit
+        @user = User.find(params[:id])
+        #@user.update(user_params)
+    end
+
+    def update
+        @user = User.find(params[:id])
+        @user.update(user_params)
+    end 
+    
+
     def user_params
         params.require(:user).permit(:username,:email,:password)
     end
