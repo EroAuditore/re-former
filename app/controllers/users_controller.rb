2363a4c98ad5afeba0a127ba8348a6c41b2b2f2d
EroAuditore <alfa.fenrir@gmail.com>
2021-03-25 10:39:14 -0600

    partial and message errors added

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,6 +10,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      flash[:notice] = "user was created successfully."
       redirect_to root_path
     else
       render :new
